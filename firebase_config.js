10b9786f78affb83e02675051e63b151b8657316
김수민 <sumin@gimsumin-ui-noteubug.local>
2024-11-09 14:12:13 +0900

    d

--- firebase_config.js
+++ firebase_config.js
@@ -1,22 +1,51 @@
-// Import the functions you need from the SDKs you need
-require('dotenv').config();  // .env 파일을 로드
+window.FIREBASE_MODULES = window.FM = {};
 
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+async function loadFirebaseModule(serviceName, sinkErrors
+) {
+    const name = serviceName.toLowerCase();
+    if (window.FIREBASE_MODULES[name]) return window.FIREBASE_MODULES[name];
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.FIREBASE_APP_ID,
-    measurementId: process.env.FIREBASE_MEASUREMENT_ID,
-};
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+    try {
+        // uses unpkg to get the latest semver
+        if (!loadFirebaseModule.version) {
+            const response = await fetch("https://unpkg.com/firebase/firebase-app.js", {method: "HEAD"});
+            const match = /@\d+(?:\.\d+)*/.exec(response.url);
+            if (!match) {
+                console.log("Unexpected resource URL (SemVer could not be determined, falling back to v9.0.0): " + response.url);
+                loadFirebaseModule.version = "9.0.0";
+            } else {
+                loadFirebaseModule.version = match[0].slice(1);
+            }
+        }
+
+        // use the found semver to pull from Google's CDN
+        const module = await import(`https://www.gstatic.com/firebasejs/${loadFirebaseModule.version}/firebase-${name}.js`);
+        window.FIREBASE_MODULES[name] = module;
+
+        console.log(`Successfully imported "${name}" module`)
+        return module;
+    } catch (err) {
+        if (sinkErrors) {
+            console.error(`Failed to import "${name}" module`, err);
+        } else {
+            throw err;
+        }
+    }
+}
+window.loadFirebaseModule = loadFirebaseModule;
+
+loadFirebaseModule('app')
+    .then(({initializeApp}) => {
+        initializeApp({
+            apiKey: "AIzaSyAgG4H6wjiZkck0Uz3wyDyu8SwFAYyCneQ",
+            authDomain: "onlineeth-6dcea.firebaseapp.com",
+            projectId: "onlineeth-6dcea",
+            storageBucket: "onlineeth-6dcea.firebasestorage.app",
+            messagingSenderId: "52648731675",
+            appId: "52648731675",
+            measurementId: "52648731675"
+        });
+    });
+
+loadFirebaseModule('storage');
+loadFirebaseModule('storage', true); // import & ignore the error
