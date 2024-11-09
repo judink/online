f2a9a621a94460385dd929ead394a92d3a190cfa
김수민 <sumin@gimsumin-ui-noteubug.local>
2024-11-09 16:15:10 +0900

    d" "d"

--- script.js
+++ script.js
@@ -1,19 +1,65 @@
-
-const socket = io('https://moniomok.xyz:3001'); // 수정예정
-
-window.requestAnimFrame = (function(){
-  return  window.requestAnimationFrame       ||
-          window.webkitRequestAnimationFrame ||
-          window.mozRequestAnimationFrame    ||
-          function(callback){
-            window.setTimeout(callback, 1000 / 60);
-          };
-})();
-
-// main.js (or other scripts)
-import { initializeFirebase } from './firebase_config.js';
-
+window.FIREBASE_MODULES = window.FM = {};
 let db, collection, getDocs, addDoc, doc, updateDoc, deleteDoc, getDoc, Timestamp;
+
+async function loadFirebaseModule(serviceName, sinkErrors
+) {
+    const name = serviceName.toLowerCase();
+    if (window.FIREBASE_MODULES[name]) return window.FIREBASE_MODULES[name];
+
+    try {
+        // uses unpkg to get the latest semver
+        if (!loadFirebaseModule.version) {
+            const response = await fetch("https://unpkg.com/firebase/firebase-app.js", {method: "HEAD"});
+            const match = /@\d+(?:\.\d+)*/.exec(response.url);
+            if (!match) {
+                console.log("Unexpected resource URL (SemVer could not be determined, falling back to v9.0.0): " + response.url);
+                loadFirebaseModule.version = "9.0.0";
+            } else {
+                loadFirebaseModule.version = match[0].slice(1);
+            }
+        }
+
+        // use the found semver to pull from Google's CDN
+        const module = await import(`https://www.gstatic.com/firebasejs/${loadFirebaseModule.version}/firebase-${name}.js`);
+        window.FIREBASE_MODULES[name] = module;
+
+        console.log(`Successfully imported "${name}" module`)
+        return module;
+    } catch (err) {
+        if (sinkErrors) {
+            console.error(`Failed to import "${name}" module`, err);
+        } else {
+            throw err;
+        }
+    }
+}
+window.loadFirebaseModule = loadFirebaseModule;
+
+
+
+async function initializeFirebase() {
+    const { initializeApp } = await loadFirebaseModule('app');
+    loadFirebaseModule('app')
+        .then(({initializeApp}) => {
+            initializeApp({
+                apiKey: "AIzaSyAgG4H6wjiZkck0Uz3wyDyu8SwFAYyCneQ",
+                authDomain: "onlineeth-6dcea.firebaseapp.com",
+                projectId: "onlineeth-6dcea",
+                storageBucket: "onlineeth-6dcea.firebasestorage.app",
+                messagingSenderId: "52648731675",
+                appId: "52648731675",
+                measurementId: "52648731675"
+            });
+        });
+
+    const firestoreModule = await loadFirebaseModule('firestore');
+    const { getFirestore, collection, getDocs, addDoc, doc, updateDoc, deleteDoc, getDoc, Timestamp } = firestoreModule;
+
+    db = getFirestore();
+    return { db, collection, getDocs, addDoc, doc, updateDoc, deleteDoc, getDoc, Timestamp };
+}
+
+
 
 initializeFirebase().then(({ db: _db, collection: _collection, getDocs: _getDocs, addDoc: _addDoc, doc: _doc, updateDoc: _updateDoc, deleteDoc: _deleteDoc, getDoc: _getDoc, Timestamp: _Timestamp }) => {
     // Firebase is initialized, you can now use the functions
@@ -29,6 +75,22 @@ initializeFirebase().then(({ db: _db, collection: _collection, getDocs: _getDocs
 
     // Proceed with your logic now
 });
+
+
+const socket = io('https://moniomok.xyz:3001'); // 수정예정
+
+window.requestAnimFrame = (function(){
+  return  window.requestAnimationFrame       ||
+          window.webkitRequestAnimationFrame ||
+          window.mozRequestAnimationFrame    ||
+          function(callback){
+            window.setTimeout(callback, 1000 / 60);
+          };
+})();
+
+// main.js (or other scripts)
+
+
 
 
 
